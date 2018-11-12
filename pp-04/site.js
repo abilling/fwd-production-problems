e362d45ccd6ea39713c30855ac924ac7257c4010
abilling <abilling@hawk.iit.edu>
2018-11-11 20:26:18 -0600

    Part 3 complete, needs testing.

--- pp-04/site.js
+++ pp-04/site.js
@@ -2,3 +2,7 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+addEventListener("click", function()){
+                      alert("Someone's at the door!") 
+                      doorbell.play();
+                                           }
